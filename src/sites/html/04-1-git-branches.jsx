1de084419d4757a39e4201e12bfb3f4756343dd1
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-08-30 14:48:32 +0200

    Created branches.

---
+++ src/sites/html/04-1-git-branches.jsx
@@ -0,0 +1,14 @@
+import Chapter from "../../components/Chapter"
+import Code from "../../components/Code"
+import Example from "../../components/Example"
+import ICode from "../../components/ICode"
+import LCode from "../../components/LCode"
+import Tag from "../../components/Tag"
+
+export default function ChapterGitBranches() {
+  return (
+    <Chapter hash={"git-branches"}>
+      <h2>Versionsverzweigungen (<ICode>branches</ICode>)</h2>
+    </Chapter>
+  )
+}
