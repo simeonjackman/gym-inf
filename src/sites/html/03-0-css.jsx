2ce47fce1494fbdbe126187c9d39571c89c00b1f
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-11-27 18:24:28 +0100

    More chapters styled.

--- src/sites/html/03-0-css.jsx
+++ src/sites/html/03-0-css.jsx
@@ -16,70 +16,69 @@ export default function ChapterCSS() {
       </section>
       <section>
         <h3>Stylesheet einbinden</h3>
-        <p>
-          Um eine saubere Trennung von Struktur und Gestaltung zu erreichen,
-          können Stylesheets in Webseiten eingebunden werden. Damit können
-          Styles auch auf mehreren Webseiten verwendet werden, und müssen nicht
-          immer wiederholt werden.
-        </p>
-        <Example title="Aufgabe: Stylesheet verlinken">
-          Fügen Sie den folgenden Code im <Tag>head</Tag> von Ihrem HTML
-          Dokument ein.
-          <LCode content='<link rel="stylesheet" href="style.css">'></LCode>
-          <p>
-            Erstellen Sie dann eine neue Datei mit dem Namen{" "}
-            <ICode>style.css</ICode>
-          </p>
-        </Example>
+        Um eine saubere Trennung von Struktur und Gestaltung zu erreichen,
+        können Stylesheets in Webseiten eingebunden werden. Damit können Styles
+        auch auf mehreren Webseiten verwendet werden, und müssen nicht immer
+        wiederholt werden.
       </section>
+      <Example title="Aufgabe: Stylesheet verlinken">
+        Fügen Sie den folgenden Code im <Tag>head</Tag> von Ihrem HTML Dokument
+        ein.
+        <Code
+          language="html"
+          content='<link rel="stylesheet" href="style.css">'></Code>
+        Erstellen Sie dann eine neue Datei mit dem Namen{" "}
+        <ICode>style.css</ICode>
+      </Example>
       <section>
         <h3>Stylesheet verwenden</h3>
-        <p>
-          Haben Sie das Stylesheet eingebunden, möchten wir als erstes
-          ausprobieren ob es funktioniert hat. Am einfachsten geht das wenn Sie
-          eine direkt sichtbare Änderung machen. Zum Beispiel können Sie die
-          Hintergrundfarbe der Webseite ändern.
-        </p>
-        <Example title="Aufgabe: Neue Hintergrundfarbe">
-          Öffnen Sie <ICode>style.css</ICode> und geben Sie den folgenden Code
-          ein:
-          <LCode
-            content={`body {
-  background-color: red;
-}`}></LCode>
-          Der Hintergrund Ihrer ganzen Seite, sollte nun rot sein.
-        </Example>
+        Haben Sie das Stylesheet eingebunden, möchten wir als erstes
+        ausprobieren ob es funktioniert hat. Am einfachsten geht das wenn Sie
+        eine direkt sichtbare Änderung machen. Zum Beispiel können Sie die
+        Hintergrundfarbe der Webseite ändern.
       </section>
+      <Example title="Aufgabe: Neue Hintergrundfarbe">
+        Öffnen Sie <ICode>style.css</ICode> und geben Sie den folgenden Code
+        ein:
+        <Code
+          language="css"
+          content={`body {
+  background-color: red;
+}`}></Code>
+        Der Hintergrund Ihrer ganzen Seite, sollte nun rot sein.
+      </Example>
       <section>
         <h3>Weitere Elemente</h3>
         Sie können in CSS einzelne Elemente unterschiedlich gestalten, dafür
         brauchen Sie einfach weitere Regeln. Diese werden in der gleichen Datei
         eingefügt.
-        <Example title="Aufgabe: Schriftfarbe des Titels ändern">
-          Um den Titel besser hervorzuheben, möchten wir gerne die Farbe des
-          Titels ändern, aber den Rest der Seite so lassen. Verwenden Sie dafür
-          diese neue Regel.
-          <LCode
-            content={`h1 {
+      </section>
+      <Example title="Aufgabe: Schriftfarbe des Titels ändern">
+        Um den Titel besser hervorzuheben, möchten wir gerne die Farbe des
+        Titels ändern, aber den Rest der Seite so lassen. Verwenden Sie dafür
+        diese neue Regel.
+        <Code
+          language="css"
+          content={`h1 {
   color: whitesmoke;
   text-decoration: underline;
   text-align: center;
-}`}></LCode>
-          Sie sollten nun einen zentrierten Titel haben der unterstrichen ist.
-        </Example>
-      </section>
+}`}
+        />
+        Sie sollten nun einen zentrierten Titel haben der unterstrichen ist.
+      </Example>
       <section>
         <h3>Mit CSS Regeln experimentieren</h3>
         Wie Sie gesehen haben, kann man verschiedene Regeln auf unterschiedliche
         Elemente anwenden. Versuchen Sie mithilfe des Editors eine weitere Regel
         für ein <Tag>p</Tag>-Tag zu erstellen, und diese anders zu gestalten.
-        <Example title="Aufgabe: Webseite gestalten">
-          Testen Sie verschiedene Stile für Ihre Webseite aus. Erstellen Sie
-          jeweils einen neuen Commit, wenn Sie mit einem Stil zufrieden sind.
-          Verändern Sie Ihren Stil mindestens 3 mal, um die History von Ihrem
-          Projekt zu vergrössern.
-        </Example>
       </section>
+      <Example title="Aufgabe: Webseite gestalten">
+        Testen Sie verschiedene Stile für Ihre Webseite aus. Erstellen Sie
+        jeweils einen neuen Commit, wenn Sie mit einem Stil zufrieden sind.
+        Verändern Sie Ihren Stil mindestens 3 mal, um die History von Ihrem
+        Projekt zu vergrössern.
+      </Example>
     </Chapter>
   )
 }
--- src/sites/html/03-1-css-selectors.jsx
+++ src/sites/html/03-1-css-selectors.jsx
@@ -10,94 +10,95 @@ export default function ChapterCSSSelectors() {
     <Chapter hash={"css-selectors"}>
       <h2>CSS Selektoren</h2>
       <section>
-        <p>
-          CSS ist von der Syntax her eine sehr einfache Sprache. CSS hat Regeln,
-          die immer im gleichen Style aufgebaut sind.
-        </p>
-        <LCode
+        CSS ist von der Syntax her eine sehr einfache Sprache. CSS hat Regeln,
+        die immer im gleichen Style aufgebaut sind.
+        <Code
+          language="css"
           content={`selector {
   property: value;
-}`}></LCode>
-        <p>
-          Die Teile in dem Block, sind selbsterklärend. Mit
-          <ICode>property</ICode> sagt man welche <b>Eigenschaft</b> man
-          verändern möchte, und <ICode>value</ICode> ist der <b>Wert</b>
-          für diese Eigenschaft. Eine grüne Schriftfarbe erreicht man zum
-          Beispiel mit
-          <ICode>color: green;</ICode>.
-        </p>
-        <p>
-          Mit dem <ICode>selector</ICode> kann man sagen für welche Elemente die
-          Regeln gelten sollen. Da haben wir bereits gesehen das man Tags
-          verwenden kann (<Tag>h1</Tag>, <Tag>body</Tag>, ...). Man kann aber
-          auch Klassen und IDs verwenden.
-        </p>
+}`}
+        />
+      </section>
+      <section>
+        Die Teile in dem Block, sind selbsterklärend. Mit
+        <ICode>property</ICode> sagt man welche <b>Eigenschaft</b> man verändern
+        möchte, und <ICode>value</ICode> ist der <b>Wert</b>
+        für diese Eigenschaft. Eine grüne Schriftfarbe erreicht man zum Beispiel
+        mit <ICode>color: green;</ICode>.
+      </section>
+      <section>
+        Mit dem <ICode>selector</ICode> kann man sagen für welche Elemente die
+        Regeln gelten sollen. Da haben wir bereits gesehen das man Tags
+        verwenden kann (<Tag>h1</Tag>, <Tag>body</Tag>, ...). Man kann aber auch
+        Klassen und IDs verwenden.
       </section>
       <section>
         <h3>CSS Klassen</h3>
-        Klassen werden vermutlich am häufigsten verwendet in CSS. Klassen werden
-        als Attribut in HTML (z.B.{" "}
-        <ICode>
-          <p class="important">Sehr wichtig</p>
-        </ICode>
-        ). In CSS gibt uns das die Möglichkeit andere Stile für <Tag>p</Tag>
+        Klassen werden vermutlich am häufigsten verwendet in CSS. In HTML werden
+        Klassen als Attribut gesetzt, wie zum Beispiel:
+        <Code
+          language="html"
+          content={`<p class="important">Sehr wichtig</p>`}
+        />
+        In CSS gibt uns das die Möglichkeit andere Stile für <Tag>p</Tag>
         -Tags anzugeben, die ebenfalls die Klasse <ICode>wichtig</ICode> haben.
-        <Example title="Aufgabe: Klassenselektor verwenden">
-          Erstellen Sie eine neue Regel in Ihrem CSS, die einen Klassenselektor
-          verwendet. Sie können Sich dabei an dem folgenden Beispiel
-          orientieren.
-          <LCode
-            content={`.important {
+      </section>
+      <Example title="Aufgabe: Klassenselektor verwenden">
+        Erstellen Sie eine neue Regel in Ihrem CSS, die einen Klassenselektor
+        verwendet. Sie können Sich dabei an dem folgenden Beispiel orientieren.
+        <Code
+          language="css"
+          content={`.important {
   border: 5px solid orange;
-}`}></LCode>
-        </Example>
+}`}
+        />
+      </Example>
+      <section>
         Um in CSS zu sagen dass es sich um eine Klasse handelt, müssen Sie
         einfach einen Punkt vor den Klassennamen schreiben.
       </section>
       <section>
         <h3>CSS IDs</h3>
-        <p>
-          Es können nicht nur Klassen sondern auch IDs verwendet werden. Dies
-          wird oftmals gemacht wenn es sich um ein eindeutiges Element handelt,
-          wie zum Beispiel eine Navigationsliste.
-        </p>
-        <p>
-          Ähnlich wie bei Klassen, muss auch nur ein spezielles Zeichen vor den
-          Namen gestellt werden. Bei IDs wird das Zeichen <ICode>#</ICode>{" "}
-          verwendet.
-        </p>
-        <Example title="Aufgabe: IDs verwenden">
-          Geben Sie einem Element auf Ihrer Webseite eine ID, und erstellen Sie
-          eine Regel für dieses Element.
-        </Example>
+        Es können nicht nur Klassen sondern auch IDs verwendet werden. Dies wird
+        oftmals gemacht wenn es sich um ein eindeutiges Element handelt, wie zum
+        Beispiel eine Navigationsliste.
       </section>
       <section>
-        <h3>Pseudo-Klassen</h3>
-        <p>
-          In CSS gibt es bestimmte bereits vordefinierte Pseudo-Klassen. Diese
-          können eine Element auswählen das zur Zeit gerade den Cursor auf sich
-          hat.
-        </p>
-        <Example title="Beispiel: Elemente vergrössern">
-          Um einen Link besser hervorzuheben, können Sie die Schriftgrösse
-          anpassen wenn der Mauszeiger über dem Link ist.
-          <LCode
-            content={`a:hover {
-  font-size: 1.4rem;
-}`}></LCode>
-          Sie können das <ICode>:hover</ICode> zu jedem Element dass Sie
-          hervorheben möchten hinzufügen.
-        </Example>
-        <p>
-          Pseudo-Klassen beginnen jeweils mit einem <ICode>:</ICode>, wie zum
-          Beispiel <ICode>:visited</ICode>.
-        </p>
-        <Example title="Aufgabe: Besuchte Links verstecken">
-          Normalerweise sind Links die Sie bereits besucht haben einfach violet.
-          Versuchen Sie mit der Pseudo-Klasse <ICode>:visited</ICode> einen Link
-          zu verstecken.
-        </Example>
+        Ähnlich wie bei Klassen, muss auch nur ein spezielles Zeichen vor den
+        Namen gestellt werden. Bei IDs wird das Zeichen <ICode>#</ICode>{" "}
+        verwendet.
       </section>
+      <Example title="Aufgabe: IDs verwenden">
+        Geben Sie einem Element auf Ihrer Webseite eine ID, und erstellen Sie
+        eine Regel für dieses Element.
+      </Example>
+      <section>
+        <h3>Pseudo-Klassen</h3>
+        In CSS gibt es bestimmte bereits vordefinierte Pseudo-Klassen. Diese
+        können eine Element auswählen das zur Zeit gerade den Cursor auf sich
+        hat.
+      </section>
+      <Example title="Beispiel: Elemente vergrössern">
+        Um einen Link besser hervorzuheben, können Sie die Schriftgrösse
+        anpassen wenn der Mauszeiger über dem Link ist.
+        <Code
+          language="css"
+          content={`a:hover {
+  font-size: 1.4rem;
+}`}
+        />
+        Sie können das <ICode>:hover</ICode> zu jedem Element dass Sie
+        hervorheben möchten hinzufügen.
+      </Example>
+      <section>
+        Pseudo-Klassen beginnen jeweils mit einem <ICode>:</ICode>, wie zum
+        Beispiel <ICode>:visited</ICode>.
+      </section>
+      <Example title="Aufgabe: Besuchte Links verstecken">
+        Normalerweise sind Links die Sie bereits besucht haben einfach violet.
+        Versuchen Sie mit der Pseudo-Klasse <ICode>:visited</ICode> einen Link
+        zu verstecken.
+      </Example>
     </Chapter>
   )
 }
