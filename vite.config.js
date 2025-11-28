9b2926015eb9c83c35b29db3c6e907ec75beebaf
Simeon Jackman <simeon@jackman.ch>
2025-11-28 14:11:35 +0100

    CHANGE: base url

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import mdx from '@mdx-js/rollup'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import mdx from "@mdx-js/rollup"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    {enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},
-    react()
+    { enforce: "pre", ...mdx(/* jsxImportSource: …, otherOptions… */) },
+    react(),
   ],
   build: {
-    outDir: 'docs',
+    outDir: "docs",
   },
-  base: "/gym-inf/",
+  base: "/",
   // assetsInclude: ["**/*.html"]
 })
